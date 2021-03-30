cf875046350e44077b7c55e311e6feb11c554fed
Sergei Savin <alter4444@gmail.com>
2021-03-30 23:02:02 +0400

    rebuild auth script

--- enter.php
+++ enter.php
@@ -1,55 +1,44 @@
 <?php
-header("Access-Control-Allow-Origin:*");
-include_once "dbsetting.php";
-$mysql= new mysqli($dbhost,$logindb,$passdb,$dbname);
-if($mysql->connect_errno)die("error db:".$mysql->connect_error);
+include_once "headers.php";
+include_once "dbsetting_n_connect.php";
 
-$auth=array("auth"=>false,"login"=>"");
-//echo 'getlogin: '.$_POST['login'].' getpass: '.$_POST['password'];
-$login = null;
-if($_GET["login"]=="ssv"){
-    $query = "SELECT * FROM `users` WHERE `login`='ssv'";
-    $login = 'ssv';
 
-}else if ($_GET["login"]=="msn"){
-    $query = "SELECT * FROM `users` WHERE `login`='msn'";
-    $login = 'msn';
-}else if ($_GET["login"]=="akellared"){
-    $query = "SELECT * FROM `users` WHERE `login`='akellared'";
-    $login = 'akellared';
-}else{
-    $auth['auth']=false;
-    $auth["login"]=$login;
-    $auth["msg"]="Пользователь ".$login." не найден";
-    echo json_encode($auth);
-    die();
-}
-$res = $mysql->query($query);
-$row = $res->fetch_assoc();
-$pass = null;
-$user_id=0;
-$user_title="";
-while($row){
-    $pass = $row['password'];
-    $email = $row['email'];
-    $user_id = $row['id'];
-    $user_title = $row['title'];
-    //echo 'password of user '.$login.': '.$pass;
-    $row = $res->fetch_assoc();
-}
-$res->close();
-$mysql->close();
-if($_GET['password']==$pass){
-    $auth['auth']=true;
-    $auth["login"]=$login;
-    $auth["id"] = $user_id;
-    $auth["user_title"] = $user_title;
-    $auth["msg"]="Авторизация для Пользователя ".$login." прошла успешно";
-    echo json_encode($auth);
-}
-else{
-    $auth['auth']=false;
-    $auth["login"]=$login;
-    $auth["msg"]="Неверный пароль для Пользователя ".$login;
-    echo json_encode($auth);
-}
+// GETTERS
+if ( $_SERVER['REQUEST_METHOD'] === 'GET' ) {
+
+    $login = isset($_GET['login']) ? $_GET['login'] : NULL;
+    $password = isset($_GET['password']) ? $_GET['password'] : NULL;
+    $q = "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'";
+
+    $auth=array("auth" => false, "login" => "");
+    
+    $res = $mysql->query( $q );
+
+    if($mysql->error) echo json_encode($mysql->error);
+
+    $row = $res ? $res->fetch_assoc() : false;
+    
+
+    if($row) {
+        $auth['auth'] = true;
+        $auth["login"] = $login;
+        $auth["msg"] = "Авторизация для Пользователя ".$login." прошла успешно";
+        $auth["email"] = $row['email'];
+        $auth["id"] = $row['id'];
+        $auth["title"] = $row['title'];
+    } else {
+        $auth["login"]=$login;
+        $auth["msg"]="Неверный пароль для Пользователя ".$login;
+        echo json_encode($auth);
+    }
+
+    echo json_encode( $auth );
+    $res->close();
+    $mysql->close();
+} else {
+    die(
+        json_encode( 
+            array("msg" => "Другие запросы кроме GET запрещены")
+        )
+    );
+} 
