54722a623d8d70642ff4d26e1aa3666a5a830b3c
Sergei Savin <alter4444@gmail.com>
2021-04-02 15:56:54 +0400

    fix

--- accessory/inventory.php
+++ accessory/inventory.php
@@ -37,7 +37,7 @@ function removeItem($id){
 function createNewSlotByUser( $_owner ){
   $query = "INSERT INTO `object_slots` (`owner`, `owner_type`) VALUES ( \"$_owner\", \"user\" )";
   //Берем последний id созданного
-  $query2 = "SELECT * FROM `object_slots` ORDER BY `datetime_update` DESC LIMIT 1";
+  $query2 = "SELECT `id` FROM `object_slots` ORDER BY `datetime_update` DESC LIMIT 1";
 
   global $mysql;
   $mysql->query($query);
@@ -52,7 +52,7 @@ function createNewSlotByUser( $_owner ){
 function createNewSlot(){
   $query = "INSERT INTO `object_slots` (`owner_type`) VALUES ( \"map\" )";
   //Берем последний id созданного
-  $query2 = "SELECT * FROM `object_slots` ORDER BY `datetime_update` DESC LIMIT 1";
+  $query2 = "SELECT `id` FROM `object_slots` ORDER BY `datetime_update` DESC LIMIT 1";
 
   global $mysql;
   $mysql->query($query);
@@ -81,7 +81,7 @@ function craftNewRGO( $_object_id, $_creator_name, $_slot){
   //создаем новый RGO
   $query = "INSERT INTO `real_game_objects` (`object_id`, `creator_name`) VALUES ( $_object_id, \"$_creator_name\" )";
   //Берем последний id созданного
-  $query2 = "SELECT * FROM `real_game_objects` ORDER BY `datetime_update` DESC LIMIT 1";
+  $query2 = "SELECT `id` FROM `real_game_objects` ORDER BY `datetime_update` DESC LIMIT 1";
 
   global $mysql;
   $mysql->query($query);
@@ -98,7 +98,7 @@ function spawnNewRGO( $_object_id ){
   //создаем новый RGO
   $query = "INSERT INTO `real_game_objects` (`object_id`) VALUES ( $_object_id )";
   //Берем последний id созданного
-  $query2 = "SELECT * FROM `real_game_objects` ORDER BY `datetime_update` DESC LIMIT 1";
+  $query2 = "SELECT `id` FROM `real_game_objects` ORDER BY `datetime_update` DESC LIMIT 1";
 
   global $mysql;
   $mysql->query($query);
